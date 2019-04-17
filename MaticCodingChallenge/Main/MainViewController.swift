4a57c24d638346518161bc7cd7183a64363fd7bc
John <john.geagea@lau.edu>
2019-04-17 09:19:12 +0300

    created table view and repo table view cell

--- MaticCodingChallenge/Main/MainViewController.swift
+++ MaticCodingChallenge/Main/MainViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
---
+++ MaticCodingChallenge/Main/Repo Table View Cell/RepoTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  RepoTableViewCell.swift
+//  MaticCodingChallenge
+//
+//  Created by John on 4/17/19.
+//  Copyright © 2019 John Geagea. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
