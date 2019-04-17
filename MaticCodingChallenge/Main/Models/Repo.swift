10aa63039bd7620852b7d0ed68bb9a2356ec7a68
John <john.geagea@lau.edu>
2019-04-17 11:41:03 +0300

    Created Repo model

---
+++ MaticCodingChallenge/Main/Models/Repo.swift
@@ -0,0 +1,23 @@
+//
+//  Repo.swift
+//  MaticCodingChallenge
+//
+//  Created by John on 4/17/19.
+//  Copyright © 2019 John Geagea. All rights reserved.
+//
+
+import UIKit
+
+class Repo: NSObject {
+    
+    var repoName : String!;
+    
+    var repoDescription : String!;
+    
+    var repoOwnerName : String!;
+    
+    var repoNumberOfStars : Int!;
+    
+    var repoUserAvatarImage : String!;
+
+}
